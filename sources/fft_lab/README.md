3987e34757970baa773d7476c47fcbcb646752eb
abuqiqi <10195102506@stu.ecnu.edu.cn>
2023-06-04 23:59:09 +0800

    fft_lab init
    初始化fft_lab

---
+++ sources/fft_lab/README.md
@@ -0,0 +1,30 @@
+# 工程目录说明
+
+本文件夹下为参赛项目代码。
+
+## 目录结构
+
+目录结构如下。
+
+```console
+├── aie
+│   ├── constraints
+│   ├── data
+│   ├── Makefile
+│   └── src
+├── host
+│   └── Makefile
+├── hw_link
+│   └── config.cfg
+├── Makefile
+└── pl
+    ├── Makefile
+    └── src
+```
+
+## 代码运行说明
+
+```bash
+make
+make aieemu
+```
---
+++ sources/fft_lab/aie/src/graph.hpp
@@ -0,0 +1,40 @@
+// Copyright (C) 2023 Advanced Micro Devices, Inc
+//
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+#include "adf.h"
+#include "fft.hpp"
+
+class SpectrumGraph: public graph {
+    private:
+        FFT1d_graph fft;
+
+    public:
+        adf::port<input> in;
+        adf::port<output> out;
+        SpectrumGraph() {
+            connect<stream>(in, fft.in);
+            // connect< window<FFT_POINT_SIZE * 4> > (fir.out,  fft.in);
+            connect<stream>(fft.out, out);
+        }
+};
+
+
+class DSPLibGraph: public graph {
+    private:
+        FFT1d_graph fft;
+        SpectrumGraph spectrum;
+        
+    public:
+        adf::input_plio s_in[4];
+        adf::output_plio s_out[6];
+        DSPLibGraph() {
+            // FFT connections
+            s_in[1] = input_plio::create("DataInFFT0", adf::plio_128_bits, "data/DataInFFT0.txt");
+            s_out[1] = output_plio::create("DataOutFFT0", adf::plio_128_bits, "DataOutFFT0.txt");
+            adf::connect<stream> (s_in[1].out[0],  fft.in);
+            adf::connect<stream> (fft.out, s_out[1].in[0]);
+        }
+};
